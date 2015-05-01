5f471e0dd349248c69b97c12d93f273d82adb256
Eric Cecchi <eric.cecchi@gmail.com>
2015-05-01 17:18:42 -0500

    Add ignored files. Oops.

---
+++ vagrant/puppet/modules/wordpress/manifests/init.pp
@@ -0,0 +1,15 @@
+# Install latest Wordpress
+
+class wordpress::install {
+
+  # Create the Wordpress database
+  exec { 'create-database':
+    unless  => '/usr/bin/mysql -u root -pvagrant wordpress',
+    command => '/usr/bin/mysql -u root -pvagrant --execute=\'create database wordpress\'',
+  }
+
+  exec { 'create-user':
+    unless  => '/usr/bin/mysql -u wordpress -pwordpress wordpress',
+    command => '/usr/bin/mysql -u root -pvagrant --execute="GRANT ALL PRIVILEGES ON wordpress.* TO \'wordpress\'@\'localhost\' IDENTIFIED BY \'wordpress\'"',
+  }
+}
