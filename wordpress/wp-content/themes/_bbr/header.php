417069b006729f1f9421324bb2e7d8b83a64f51f
Zach Reed <zreed@bluetideproductions.com>
2015-05-03 02:42:02 -0500

    PayPal btns

--- wordpress/wp-content/themes/_bbr/header.php
+++ wordpress/wp-content/themes/_bbr/header.php
@@ -79,10 +79,21 @@ global $_bbr_admin;
           </h1>
         </div>
         <?php
-          if($_bbr_admin['opt-header-button-callout-url'] && $_bbr_admin['opt-header-button-callout-text']){
-            echo '<a href="'.$_bbr_admin['opt-header-button-callout-url'].'" class="btn btn-primary pull-right push-top hidden-xs hidden-sm">'.$_bbr_admin['opt-header-button-callout-text'].'</a>';
-          }
+          // if($_bbr_admin['opt-header-button-callout-url'] && $_bbr_admin['opt-header-button-callout-text']){
+          //   echo '<a href="'.$_bbr_admin['opt-header-button-callout-url'].'" class="btn btn-primary pull-right push-top hidden-xs hidden-sm">'.$_bbr_admin['opt-header-button-callout-text'].'</a>';
+          // }
         ?>
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class=" pull-right push-top hidden-xs hidden-sm" style="margin-top:12px;">
+          <input type="hidden" name="cmd" value="_donations">
+          <input type="hidden" name="business" value="paypal@brownbabyreads.com">
+          <input type="hidden" name="lc" value="US">
+          <input type="hidden" name="item_name" value="Brown Baby Reads">
+          <input type="hidden" name="no_note" value="0">
+          <input type="hidden" name="currency_code" value="USD">
+          <input type="hidden" name="bn" value="PP-DonationsBF:btn-donate.svg:NonHostedGuest">
+          <input type="image" src="http://overnight-website.s3.amazonaws.com/wp-uploads/btn-donate-red.svg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" height="40px">
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          </form>
         <a href="#" class="visible-sm visible-xs" id="menu-toggle"><i class="fa fa-bars"></i></a>
       </div>
     </header>
--- wordpress/wp-content/themes/_bbr/page-donate.php
+++ wordpress/wp-content/themes/_bbr/page-donate.php
@@ -16,7 +16,17 @@ while (have_posts() ) : the_post();
         <div class="col-md-6 col-md-offset-3">
           <h2>We appreciate you!</h2>
           <p>Our financial support from foundations, organizations and individuals allows Brown Bay Reads to maintain it’s website and expand programming.</p>
-          <a href="#" class="btn btn-primary btn-lg">Donate Now</a>
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_donations">
+            <input type="hidden" name="business" value="paypal@brownbabyreads.com">
+            <input type="hidden" name="lc" value="US">
+            <input type="hidden" name="item_name" value="Brown Baby Reads">
+            <input type="hidden" name="no_note" value="0">
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="hidden" name="bn" value="PP-DonationsBF:btn-donate.svg:NonHostedGuest">
+            <input type="image" src="http://overnight-website.s3.amazonaws.com/wp-uploads/btn-donate-red.svg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" height="40px">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+            </form>
         </div>
       </div>
     </div>
