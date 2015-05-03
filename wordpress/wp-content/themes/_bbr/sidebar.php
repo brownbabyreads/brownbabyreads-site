5e706468061db976a3a41bebe042ae03e67caf17
Eric Cecchi <eric.cecchi@gmail.com>
2015-05-03 01:08:38 -0500

    Fix default page view and some other shit

--- wordpress/wp-content/themes/_bbr/sidebar.php
+++ wordpress/wp-content/themes/_bbr/sidebar.php
@@ -10,19 +10,17 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</div><!-- #secondary -->
-
 <div class="widget sidebar-widget box-style1">
-	<h3 class="widget-title">Categories</h3>
+  <?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+<!-- 	<h3 class="widget-title">Categories</h3>
 	<ul>
 		<?php
 			$args = [
 				'title_li' => null,
 			];
-			wp_list_categories( $args ); 
+			wp_list_categories( $args );
 		?>
 
-	</ul>
+	</ul> -->
 </div>
