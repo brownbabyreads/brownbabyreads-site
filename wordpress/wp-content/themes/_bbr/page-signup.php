a9759dcfa7d078d37b2e20160852efdc723f9f8a
Eric Cecchi <eric.cecchi@gmail.com>
2015-05-03 06:51:09 -0500

    Kill filler content on signup

--- wordpress/wp-content/themes/_bbr/page-signup.php
+++ wordpress/wp-content/themes/_bbr/page-signup.php
@@ -13,20 +13,6 @@ the_post();
     <div class="container">
       <div class="row">
         <div class="col-md-9">
-          <h4 class="short accent-color">Join Vestige</h4>
-          <h2>Members help sustain this world-class institution while enjoying many outstanding benefits throughout the year.</h2>
-          <p><strong>All Members receive:</strong></p>
-          <ul class="angles">
-            <li>Free admission to the Main Building and The Cloisters museum and gardens</li>
-            <li>Members-only emails with advance notice of events, programs, and offers</li>
-            <li>Access to the exclusive Members Dining Room</li>
-            <li>Discounts in The Shop Pleu (plus seasonal Double Discounts), and on Audio Guides and Museum parking</li>
-            <li>Eligibility to rent elegant museum spaces for private events</li>
-            <li>Special benefits and features in the Members-only section of the Museum's website</li>
-          </ul>
-          <hr class="fw">
-          <div class="spacer-30"></div>
-
           <?php the_content(); ?>
         </div>
       </div>
