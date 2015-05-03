af352a1dd214b319106e8dcf82bf1aa28dcfc5cb
Matt Born <me+code@mattborn.com>
2015-05-02 21:35:37 -0500

    Update catch-all page template

--- wordpress/wp-content/themes/_bbr/page.php
+++ wordpress/wp-content/themes/_bbr/page.php
@@ -12,24 +12,34 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="main" role="main">
+  <div id="content" class="content full">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9">
+          <?php if ( have_posts() ) : ?>
+          <?php /* Start the Loop */ ?>
+          <?php while ( have_posts() ) : the_post(); ?>
+          <?php
+            /* Include the Post-Format-specific template for the content.
+            * If you want to override this in a child theme, then include a file
+            * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+            */
+            get_template_part( 'template-parts/content', get_post_format() );
+          ?>
+          <?php endwhile; ?>
+          <?php the_posts_navigation(); ?>
+          <?php else : ?>
+          <?php get_template_part( 'template-parts/content', 'none' ); ?>
+          <?php endif; ?>
+        </div>
+        <div class="col-md-3 sidebar right-sidebar">
+          <?php get_sidebar(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
